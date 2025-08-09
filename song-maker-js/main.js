c74d3615e0cba4a2083a5924302848698a83820a
a-00-a <sy.lee.2391@gmail.com>
2025-08-09 21:48:28 +0900

    Session 2: learn JS

--- song-maker-js/main.js
+++ song-maker-js/main.js
@@ -1,2 +1,14 @@
+console.log("Start loading JS");
+
+const grid = document.getElementById("grid");
+
+const myTempH1 = document.createElement("h1");
+grid.appendChild(myTempH1);
+
+console.log("created myTempH1");
+
+myTempH1.textContent = "Bye bye!";
+
+console.log("Finish loading JS");
 
 
